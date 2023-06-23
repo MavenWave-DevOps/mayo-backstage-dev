5b84c9ba63868c65596830f8f72a456596ae9d1c
sankd <santosh.dubey@mavenwave.com>
2023-06-23 11:14:42 +0530

    update code in permission ts file

--- packages/backend/src/plugins/permission.ts
+++ packages/backend/src/plugins/permission.ts
@@ -1,31 +1,68 @@
 import { createRouter } from '@backstage/plugin-permission-backend';
 import {
-    AuthorizeResult,
-    PolicyDecision,
-    isPermission,
+  AuthorizeResult,
+  PolicyDecision,
+  isPermission,
 } from '@backstage/plugin-permission-common';
 import {
-    PermissionPolicy,
-    PolicyQuery,
+  PermissionPolicy,
+  PolicyQuery,
 } from '@backstage/plugin-permission-node';
 import { catalogConditions, createCatalogConditionalDecision, } from '@backstage/plugin-catalog-backend/alpha';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 import { BackstageIdentityResponse } from '@backstage/plugin-auth-node';
 import { catalogEntityReadPermission } from '@backstage/plugin-catalog-common/alpha';
+import { Config, ConfigReader } from '@backstage/config';
+import { getRootLogger, loadBackendConfig } from '@backstage/backend-common';
+
+
+// export interface ymlConfig {
+//   ADMIN: string;
+//   GROUP: string;
+//   USER: string;
+// }
+// const getymlConfig = (config: Config): ymlConfig => {
+//   return config.get<ymlConfig>('backend.env');
+// }
+
+// let rbac = getymlConfig(ConfigReader.fromConfigs());
+
+// const backendConfig = {
+//   backend: {
+//     baseUrl: 'http://localhost:7007',
+//   },
+// };
+// const Rconfig = new ConfigReader(backendConfig);
+// console.log('$##########################33$', Rconfig.getConfig('backend'));
+
+//Custome code start here
+async function main() {
+  const config = await loadBackendConfig({
+    argv: process.argv,
+    logger: getRootLogger(),
+  });
+  const itemConfig = config.getConfig('rbac-plugin.teams');
+  console.log('$$$$$$$$$$$$$$$$$$$$$$$4', itemConfig)
+}
+main().catch(error => {
+  console.error('Backend failed to start up', error);
+  process.exit(1);
+});
+//End of the custom code
 
 class AccessPolicy implements PermissionPolicy {
   async handle(
     request: PolicyQuery,
     user?: BackstageIdentityResponse
-  ):Promise<PolicyDecision> {
-    if(isPermission(request.permission,catalogEntityReadPermission)){
-      if(user?.identity.ownershipEntityRefs.includes('group:default/platform_admin')){
-        return{result: AuthorizeResult.ALLOW}
+  ): Promise<PolicyDecision> {
+    if (isPermission(request.permission, catalogEntityReadPermission)) {
+      if (user?.identity.ownershipEntityRefs.includes('group:default/ado-dev-team')) {
+        return { result: AuthorizeResult.ALLOW }
       }
 
-      if(user?.identity.ownershipEntityRefs.includes('group:default/businessb') || user?.identity.ownershipEntityRefs.includes('group:default/business_a') ){
-        return{result: AuthorizeResult.ALLOW}
+      if (user?.identity.ownershipEntityRefs.includes('group:default/businessb') || user?.identity.ownershipEntityRefs.includes('group:default/business_a')) {
+        return { result: AuthorizeResult.ALLOW }
       }
 
       return createCatalogConditionalDecision(
@@ -33,7 +70,7 @@ class AccessPolicy implements PermissionPolicy {
         anyOf: [
           {
             allOf: [
-              catalogConditions.isEntityKind({kinds:['template']}),
+              catalogConditions.isEntityKind({ kinds: ['template'] }),
               catalogConditions.isEntityOwner({
                 claims: user?.identity.ownershipEntityRefs ?? ['Group'],
               }),
@@ -42,18 +79,18 @@ class AccessPolicy implements PermissionPolicy {
         ],
       });
     }
-    return { result: AuthorizeResult.DENY };
+    return { result: AuthorizeResult.ALLOW };
   }
 }
 
 export default async function createPlugin(
-    env: PluginEnvironment,
+  env: PluginEnvironment,
 ): Promise<Router> {
-    return await createRouter({
-        config: env.config,
-        logger: env.logger,
-        discovery: env.discovery,
-        policy: new AccessPolicy(),
-        identity: env.identity,
-    });
+  return await createRouter({
+    config: env.config,
+    logger: env.logger,
+    discovery: env.discovery,
+    policy: new AccessPolicy(),
+    identity: env.identity,
+  });
 }
