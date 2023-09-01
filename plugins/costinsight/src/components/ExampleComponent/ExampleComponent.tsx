7aa8e31e67f4a74b017bf21e989cf8b55c39dfb8
sankd <santosh.dubey@mavenwave.com>
2023-09-02 01:05:03 +0530

    update code base

--- plugins/costinsight/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/costinsight/src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,118 +1,59 @@
-// import React from 'react';
-// import { Typography, Grid } from '@material-ui/core';
-// import {
-//   InfoCard,
-//   Header,
-//   Page,
-//   Content,
-//   ContentHeader,
-//   HeaderLabel,
-//   SupportButton,
-// } from '@backstage/core-components';
-// // import { ExampleFetchComponent } from '../ExampleFetchComponent';
-
-// export const ExampleComponent = () => (
-//   <Page themeId="tool">
-//     <Header title="Welcome to costinsight!" subtitle="Optional subtitle">
-//       <HeaderLabel label="Owner" value="Team X" />
-//       <HeaderLabel label="Lifecycle" value="Alpha" />
-//     </Header>
-//     <Content>
-//       <ContentHeader title="Plugin title">
-//         <SupportButton>A description of your plugin goes here.</SupportButton>
-//       </ContentHeader>
-//       <Grid container spacing={3} direction="column">
-//         <Grid item>
-//           <InfoCard title="Information card">
-//             <Typography variant="body1">
-//               All content should be wrapped in a card like this.
-//             </Typography>
-//           </InfoCard>
-//         </Grid>
-//         <Grid item>
-//           {/* <ExampleFetchComponent /> */}
-//         </Grid>
-//       </Grid>
-//     </Content>
-//   </Page>
-// );
-
-
-// import React from 'react';
-// import { useEntity } from '@backstage/plugin-catalog-react';
-// import { costinsightObject } from '../../hooks/costinsightObject';
-// // import { ExampleFetchComponent } from '../ExampleFetchComponent';
-
-// export const ExampleComponent = () => {
-//   const { entity } = useEntity();
-//   const { error, loading, status } = costinsightObject();
-//   if (loading) {
-//     return <div>Loading</div>;
-//   }
-//   if (error) {
-//     return <div>Error</div>;
-//   }
-//   return (<>
-//     <div>Hello {entity.metadata.name}</div>
-//     <div>Status: {status}</div>
-//     {/* <ExampleFetchComponent /> */}
-//   </>);
-// }
-
+import {
+  Header,
+  Page,
+  HeaderLabel,
+} from '@backstage/core-components';
+import { Grid } from '@material-ui/core';
 import React, { useEffect, useState } from "react";
-
 import { useApi } from "@backstage/core-plugin-api";
-
 import { costinsightApiRef } from "../../api";
 import { convertSchema } from "../../hooks/utility";
+import { CostGraph } from '../ExampleFetchComponent/CostGraph';
+
 
 export const ExampleComponent = () => {
 
   const bgCostApi = useApi(costinsightApiRef);
-  const [status, setStatus] = useState<string>('');
-  const [responsedata, setResponseData] = useState<any>({});
+  let [responsedata, setResponseData] = useState<any>({});
+
 
   useEffect(() => {
+
     const fetchbgCostdata = async () => {
       try {
-        const data = await bgCostApi.getHealth();
+
         const bgresponse = await bgCostApi.getResponseData();
-
         setResponseData(bgresponse);
-
-        setStatus(data.status);
       }
       catch (error) {
-        console.error('ERROR FETCHING BIGQUERY API', error);
+        console.error('Error Fetching Bigquery API', error);
       }
-
-    };
-
-    fetchbgCostdata();
-
+    }; fetchbgCostdata();
   }, []);
 
-  // const sschema = responsedata['responseData']['schema'];
-  // // var ssrows = responsedata['responseData'];
-  // console.log('Experimental Schema', sschema);
-  // // console.log('Experimental Rows', ssrows);
-  console.log('RESPONSE-DATA', responsedata);
-  const rrows = convertSchema(responsedata["responseData"]["schema"]["fields"], responsedata["responseData"]["rows"]);
-  console.log('CONVERTED', rrows);
+
+  if (Object.keys(responsedata).length !== 0) {
+    convertSchema(responsedata["responseData"]["schema"]["fields"], responsedata["responseData"]["rows"]);
+  }
+  else {
+    console.info('Response is Empty')
+  }
 
   return (
-
     <div>
       {responsedata ? (
-        <div>
-          <h1>BG BACKEND DATASET PLUGIN API</h1>
-          <p>sankd: {responsedata.length
-
-          }</p>
-        </div>
-
-      ) : (<p>status: health</p>)}
+        <Page themeId="tool">
+          <Header title="MCCP" subtitle="2023-2024" >
+            <HeaderLabel label="Environment" value="Google Cloud Resources" />
+          </Header>
+          <Grid container spacing={3}></Grid>
+          <CostGraph />
+        </Page>
+      ) : (
+        <p> Something goes wrong...</p>
+      )}
     </div>
   );
+
 };
 
---
+++ plugins/costinsight/src/components/ExampleFetchComponent/CostGraph.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from 'react';
+import Chart from "react-apexcharts";
+import { Grid, Card, CardContent, makeStyles } from "@material-ui/core";
+import { env } from 'process';
+
+
+export const CostGraph = () => {
+
+    useEffect(() => {
+        const getData = async () => {
+
+        };
+        getData();
+    }, []);
+
+    /**
+     * Y- Axis Data
+     */
+    const series = [
+
+        {
+            name: 'Q1 Budget',
+            group: 'budget',
+            data: [101000, 20200, 30030, 40200, 50000]
+        },
+        {
+            name: 'Q1 Consumed',
+            group: 'actual',
+            data: [4000, 25000]
+        },
+        {
+            name: 'Q2 Budget',
+            group: 'budget',
+            data: [12000, 42000, 38000, 89000, 88700]
+        },
+        {
+            name: 'Q2 Consumed',
+            group: 'actual',
+            data: [4000, 25000]
+        }
+    ];
+
+    /**
+     * X- Axis Data
+     */
+    const options = {
+        chart: { id: 'bar-chart', stacked: true, height: 750 },
+        stroke: {
+            width: 1,
+            colors: ['#fff'],
+        },
+        title: {
+            floating: false,
+            text: 'MCCP- Cost Analysis (March-2023 - March 2024)',
+            align: 'center',
+            offsetX: 90,
+            style: {
+                fontSize: '18px'
+            }
+        },
+        subtitle: {
+            text: "projectId",
+            align: 'center',
+            margin: 5,
+            offsetY: 40,
+            style: {
+                color: '#9574bd',
+                fontSize: '14px',
+            }
+        },
+        // dataLabels: {
+        //     formatter: (val) => { return val / 1000 + "$" }
+        // },
+        xaxis: {
+            // categories: 'service_description',
+            // tickAmount: 5,
+            // labels: {
+            //     formatter: (val) => {
+            //         return val / 1000 + '$'
+            //     }
+            // },
+        },
+
+
+        fill: {
+            opacity: 1,
+        },
+        colors: ['#80c7fd', '#008FFB', '#80f1cb', '#00E396'],
+
+        legend: {
+            position: 'top',
+            horizontalAlign: 'left'
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+                dataLables: {
+                    position: 'bottom'
+                }
+            }
+        },
+    };
+    // });
+
+
+
+    const useStyles = makeStyles((theme) => ({
+        container: {
+            backgroundColor: "grey"
+        },
+        item: {
+            width: "100%"
+        },
+        charts: {
+            flexDirection: "column"
+        },
+        card: {
+            height: "100%"
+        },
+        content: {
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+        },
+        chartWrapper: {
+            width: "95%",
+            height: "100%",
+            overflow: "hidden"
+        }
+    }));
+
+    const classes = useStyles();
+    return (
+        <Grid className={classes.container} container spacing={2}>
+            <Grid item className={classes.item}>
+                <Grid container className={classes.charts} spacing={2}>
+                    <Grid item>
+                        <Card className={classes.card}>
+                            <CardContent>
+                                <div className={classes.content}>
+                                    <div className={classes.chartWrapper}>
+                                        <Chart
+                                            options={options}
+                                            series={series}
+                                            type="bar"
+                                            height="650"
+                                        />
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
