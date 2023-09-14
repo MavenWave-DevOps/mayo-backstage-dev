1a97fd3e96c6b137edf49071d267e9b80d3873c7
sankd <santosh.dubey@mavenwave.com>
2023-09-14 18:33:44 +0530

    service ac impersonation

--- plugins/bigquery-api-backend/src/service/router.ts
+++ plugins/bigquery-api-backend/src/service/router.ts
@@ -37,21 +37,31 @@ export async function createRouter(
   const readAppConfig = config.getConfig('bigqueryapi');
   const projectId = readAppConfig.getString('projectId');
   const gbdataset = readAppConfig.getString('gbdataSet');
+  const { GoogleAuth, Impersonated } = require('google-auth-library');
+
 
   router.get('/dataset', async (_, response) => {
 
-    const { JWT } = require('google-auth-library');
-    const keys = require('../config/cedar-setup.json');
-    const client = new JWT({
-      email: keys.client_email,
-      key: keys.private_key,
-      scopes: ['https://www.googleapis.com/auth/bigquery'],
+    const scopes = 'https://www.googleapis.com/auth/cloud-platform'
+    const auth = new GoogleAuth({
+      scopes: scopes
     });
+    const client = await auth.getClient();
+    let targetPrincipal = 'googlebigquery@cedar-setxxxxxx.iam.gserviceaccount.com'
+
+    const targetClient = new Impersonated({
+      sourceClient: client,
+      targetPrincipal: targetPrincipal,
+      lifetime: 30,
+      delegates: [],
+      targetScopes: [scopes]
+    });
+
     const url = `https://bigquery.googleapis.com/bigquery/v2/projects/${projectId}/queries`
-    const res = await client.request({
+    const res = await targetClient.request({
       url, method: 'POST', headers: {
         'Content-type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer ${accessToken}'
+        'Authorization': 'Bearer ${accessToken}',
       },
       body: JSON.stringify({
         "query":
